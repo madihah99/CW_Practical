d478ca88b30bebcbc1e6a31b5fda055a7f0f98db
madihah99 <48766485+madihah99@users.noreply.github.com>
2022-11-25 21:58:40 +0000

    added all commands

--- test1.sh
+++ test1.sh
@@ -6,8 +6,74 @@ java CurrencyConverter 1 dollars
 
 echo -e
 
-java CurrencyConverter 1 pounds
+java CurrencyConverter 1 DOLLARS
+
+echo -e
+
+java CurrencyConverter 1 Dollars
+
+echo -e
+
+java CurrencyConverter 1 dollar
+
+echo -e
+
+java CurrencyConverter 1000 dollars
+
+echo -e
+
+java CurrencyConverter dollars 1
+
+echo -e
 
 # Tests for pound values
 
+java CurrencyConverter 1 pounds
+
+echo -e
+
+java CurrencyConverter 1 POUNDS
+
+echo -e
+
+java CurrencyConverter 1 Pounds
+
+echo -e
+
+java CurrencyConverter 1 pound
+
+echo -e
+
+java CurrencyConverter 1000 pounds
+
+echo -e
+
+java CurrencyConverter pounds 1
+
+echo -e
+
+
 # Tests for euro values
+
+java CurrencyConverter 1 euros
+
+echo -e
+
+java CurrencyConverter 1 EUROS
+
+echo -e
+
+java CurrencyConverter 1 Euros
+
+echo -e
+
+java CurrencyConverter 1 euro
+
+echo -e
+
+java CurrencyConverter 1000 euros
+
+echo -e
+
+java CurrencyConverter euros 1
+
